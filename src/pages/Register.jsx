66dd43a0bb69e831d89ec5bfd59944123fe88b83
Cody Clark <codyjamesclark@gmail.com>
2024-10-22 15:29:31 -0500

    login and register wokring

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -27,7 +27,7 @@ export const Register = () => {
             .then(res => res.json())
             .then(authInfo => {
                 if (authInfo && authInfo.token) {
-                    localStorage.setItem("gamer_token", JSON.stringify(authInfo))
+                    localStorage.setItem("diner_token", JSON.stringify(authInfo))
                     navigate("/")
                 } else {
                     existDialog.current.showModal()
@@ -44,7 +44,7 @@ export const Register = () => {
 
             <section>
                 <form className="form--login" onSubmit={handleRegister}>
-                    <h1 className="text-4xl mt-7 mb-3">Gamer Rater</h1>
+                    <h1 className="text-4xl mt-7 mb-3">findining</h1>
                     <h2 className="text-xl mb-10">Register new account</h2>
                     <fieldset className="mb-4">
                         <label htmlFor="firstName"> First name </label>
