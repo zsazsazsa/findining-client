a15954f01be03fc5f7f55a658e9e0278945b2f8a
Cody Clark <codyjamesclark@gmail.com>
2024-10-22 15:20:17 -0500

    initial commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+function Home() {
+    return (
+      <main className='text-slate-900 pl-10 pr-10'>
+        <h1 className='text-4xl'>findining</h1>
+      </main>
+    )
+  }
+  export default Home
+  
